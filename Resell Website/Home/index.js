26ac685e94e82818a1198d7d04d2aac41cfa46df
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-05-16 14:14:27 +1000

    Update index.js

--- Resell Website/Home/index.js
+++ Resell Website/Home/index.js
@@ -2,8 +2,19 @@
 const Question1 = document.getElementById("Question1");
 const Answer1 = document.getElementById("Answer1")
 
+
+// Question1.addEventListener("click", () => {
+//     alert("F")
+//     if (Answer1.style.display == "none") {
+//         Answer1.style.display = "block"
+//     } else {
+//         Answer1.style.display = "none"
+//     }
+// })
+
 function Open1() {
-    alert("#")
+    // alert("#")
+    Answer1.style.display = "block"
 }
 
 
