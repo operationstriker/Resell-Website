4b65cb74b9a77807a348ff7983a6b0c2c2e774be
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-05-20 11:34:26 +1000

    Update index.js

--- Resell Website/Shop/index.js
+++ Resell Website/Shop/index.js
@@ -1,9 +1,7 @@
 
 
 const location = ["Australia", "New Zealand", "United Kingdom"]
-// console.log(location[0])
 
-// console.log(defaultlocation)
 
 const PriceText = document.getElementsByClassName("PriceText")
 
