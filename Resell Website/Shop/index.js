cff463d97044a0273a2c502dc00d4849149afce0
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-05-20 11:34:13 +1000

    Update index.js

--- Resell Website/Shop/index.js
+++ Resell Website/Shop/index.js
@@ -1,10 +1,5 @@
 
 
-// const defaultcurrency = "AUD"
-// const defaultlocation = location[0]
-
-// array
-
 const location = ["Australia", "New Zealand", "United Kingdom"]
 // console.log(location[0])
 
