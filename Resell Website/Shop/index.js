e7b53dbf6c9aa4ca04735266e887f86b23016945
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-05-20 11:44:00 +1000

    Update index.js

--- Resell Website/Shop/index.js
+++ Resell Website/Shop/index.js
@@ -7,7 +7,7 @@ const PriceText = document.getElementsByClassName("PriceText")
 const AUDOption = document.getElementById("AUDOption");
 const NZOption = document.getElementById("NZOption");
 const UKOption = document.getElementById("UKOption");
-const EUOption = document.getElementById("EUOption")
+const EUOption = document.getElementById("EUOption");
 const USOption = document.getElementById("USOption")
 const CADOption = document.getElementById("CADOption");
 
